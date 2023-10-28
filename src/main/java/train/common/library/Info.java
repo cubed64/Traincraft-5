b8eac2e1b7e099bf1857d5c017266f5f207ea9fc
bidahochi <agentnolan007@gmail.com>
2023-10-28 11:37:07 -0600

    version numbers, we 1.9.0 now

--- src/main/java/train/common/library/Info.java
+++ src/main/java/train/common/library/Info.java
@@ -12,7 +12,7 @@ public class Info {
 	/* Mod relevant information */
 	public static final String modID = "tc";
 	public static final String modName = "Fox-Traincraft";
-	public static final String modVersion = "1.8.3";
+	public static final String modVersion = "1.9.0";
 	public static final String channel = "Traincraft";
 	public static final String keyChannel = "TraincraftKey";
 	public static final String rotationChannel = "TraincraftRotation";
