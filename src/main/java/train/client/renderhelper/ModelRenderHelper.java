f7edd1c62717c455070aeb64fd6182b2880820a9
TBEA <74006945+RealTBEA@users.noreply.github.com>
2024-06-22 17:23:25 -0400

    Update ModelRenderHelper.java
    Added another helper method that can be used on locomotives that don't care about beacons

--- src/main/java/train/client/renderhelper/ModelRenderHelper.java
+++ src/main/java/train/client/renderhelper/ModelRenderHelper.java
@@ -68,6 +68,37 @@ public class ModelRenderHelper
         }
     }
 
+    /** Renders a locomotive model
+     * Can Render the following spec Special items
+     * lamp,
+     * cull
+     * @param bodyModel
+     * @param locomotive
+     * @param f5
+     */
+    public static void renderLocomotiveModelWithoutBeacon(ModelRendererTurbo[] bodyModel, Locomotive locomotive, float f5)
+    {
+        for (int i = 0; i < bodyModel.length; i++)
+        {
+            if (bodyModel[i].boxName.contains("lamp") && locomotive.isLocomotiveLightsEnabled())
+            {
+                Minecraft.getMinecraft().entityRenderer.disableLightmap(1D);
+                bodyModel[i].render(f5);
+                Minecraft.getMinecraft().entityRenderer.enableLightmap(1D);
+            }
+            else if (bodyModel[i].boxName.contains("cull"))
+            {
+                GL11.glDisable(GL11.GL_CULL_FACE);
+                bodyModel[i].render(f5);
+                GL11.glEnable(GL11.GL_CULL_FACE);
+            }
+            else
+            {
+                bodyModel[i].render(f5);
+            }
+        }
+    }
+
     private static void renderPrimeLight(ModelRendererTurbo bodyModel, byte cycleIndex, float f5)
     {
         // prime1 -- Front Facing #1
