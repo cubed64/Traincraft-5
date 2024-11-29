233505024dd8aa5c05949915108bfe498642ff9d
bidahochi <agentnolan007@gmail.com>
2024-11-29 14:34:42 -0700

    fixes, version change

--- src/main/java/train/common/library/Info.java
+++ src/main/java/train/common/library/Info.java
@@ -12,7 +12,7 @@ public class Info {
 	/* Mod relevant information */
 	public static final String modID = "tc";
 	public static final String modName = "Fox-Traincraft";
-	public static final String modVersion = "2.0.0.3";
+	public static final String modVersion = "2.0.1.0";
 	public static final String channel = "Traincraft";
 	public static final String keyChannel = "TraincraftKey";
 	public static final String rotationChannel = "TraincraftRotation";
