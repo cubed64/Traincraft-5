16c13fe1419025ef2f4bc29dbaa546eaa3d9f13d
Trains from 1.3.2 to 1.7.10 <trains.from.1.3.2.to.1.7.10@gmail.com>
2025-05-31 20:51:45 -0500

    Localization moment
    -updated the localization files of the Class B climax

--- src/main/java/com/jcirmodelsquad/tcjcir/vehicles/locomotives/SteamClimaxB.java
+++ src/main/java/com/jcirmodelsquad/tcjcir/vehicles/locomotives/SteamClimaxB.java
@@ -20,7 +20,7 @@ public class SteamClimaxB extends SteamTrain {
 		textureDescriptionMap.put(0, "Hillcrest Lumber Co #9");
 		textureDescriptionMap.put(1, "Washaska Resources #6");
 		textureDescriptionMap.put(2, "Washaska Resources #7");
-		textureDescriptionMap.put(3, "CMLC");
+		textureDescriptionMap.put(3, "Clarks Milling & Lumber Co");
 	}
 
 	public void initLocoSteam() {
