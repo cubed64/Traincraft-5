e56f1acb1ca414b7d1bdae3b7ff3e337c3c96d1e
Messicrafter <benjamin.messimer@gmail.com>
2025-06-06 00:04:49 -0400

    WCFB Replaces his dash 9 with an 8
    added a new GCM skin

--- src/main/java/com/jcirmodelsquad/tcjcir/vehicles/locomotives/DieselDash840C.java
+++ src/main/java/com/jcirmodelsquad/tcjcir/vehicles/locomotives/DieselDash840C.java
@@ -26,6 +26,7 @@ public class DieselDash840C extends DieselTrain {
         textureDescriptionMap.put(6, "Morristown Tenneva & Southern");
         textureDescriptionMap.put(7, "FURRX");
         textureDescriptionMap.put(8, "CSXT (JCIR 6th Anniversary)");
+        textureDescriptionMap.put(9, "GCM");
     }
     public DieselDash840C(World world, double d, double d1, double d2){
         this(world);
