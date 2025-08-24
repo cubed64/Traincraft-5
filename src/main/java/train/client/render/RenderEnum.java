a68acca7b1b066687d9b9e83fd8a5ef04677f331
Jared <funguseater85@gmail.com>
2025-08-24 14:35:03 -0400

    Add Refrigerated Car to JCIR
    -Added PFE R-40-23 Ice Reefer
    -Added Skins for PFE R-40-23

--- src/main/java/train/client/render/RenderEnum.java
+++ src/main/java/train/client/render/RenderEnum.java
@@ -2088,6 +2088,7 @@ public enum RenderEnum
 	Wood1912Boxcar(Wood1912Boxcar.class, new Model1912WoodBoxcar(),"1912_wood_boxcar_", true, new float[]{-0F, 0.1F, 0F}, new float[] { 0F, 180F, 180F }, null, "", 0, null, "", null, 0, false),
 	PS_40t_ss_box(PS_40t_ss_box.class, new ModelPS_40t_ss_box(),"ps_40t_ss_box_", true, new float[]{-0F, 0.15F, 0F}, new float[] { 0F, 180F, 180F }, null, "", 0, null, "", null, 0, false),
 	SP_B50(SP_B50.class, new ModelSP_B50(),"sp_b_50_", true, new float[]{-0F, 0.15F, 0F}, new float[] { 0F, 180F, 180F }, null, "", 0, null, "", null, 0, false),
+	PFEr_40_23Reefer(PFEr_40_23Reefer.class, new ModelPFEr_40_23Reefer(),"PFEr_40_23Reefer_", true, new float[]{-0F, 0.15F, 0F}, new float[] { 0F, 180F, 180F }, null, "", 0, null, "", null, 0, false),
 
 	VersaLongi(VersaLongi.class, new ModelVersaLongi(),"versa_longi_", true,  new float[]{-0F, 0.10F, 0F}, new float[] { 0F, 180F, 180F }, null, "", 0, null, "", null, 0, false),
 	VersaTrans(VersaTrans.class, new ModelVersaTrans(),"versa_trans_", true, new float[]{-0F, 0.10F, 0F}, new float[] { 0F, 180F, 180F }, null, "", 0, null, "", null, 0, false),
