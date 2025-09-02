ee8287273683da262f805b91e3f0b8b9dc69ae55
TBEA <74006945+RealTBEA@users.noreply.github.com>
2025-09-02 01:03:22 -0400

    Converted Osgood To Combine car
    * Converted Osgood To Combine car

--- src/main/java/com/jcirmodelsquad/tcjcir/vehicles/rollingstock/passenger/OBBaggage_52SeatDividedCoach.java
+++ src/main/java/com/jcirmodelsquad/tcjcir/vehicles/rollingstock/passenger/OBBaggage_52SeatDividedCoach.java
@@ -2,21 +2,27 @@ package com.jcirmodelsquad.tcjcir.vehicles.rollingstock.passenger;
 
 import net.minecraft.world.World;
 import train.common.api.AbstractPassengerCar;
+import train.common.api.AbstractPassengerCombineCar;
 
-public class OBBaggage_52SeatDividedCoach extends AbstractPassengerCar
+public class OBBaggage_52SeatDividedCoach extends AbstractPassengerCombineCar
 {
 	public OBBaggage_52SeatDividedCoach(World world) {
 		super(world);
-		textureDescriptionMap.put(0, "SAL");
-		textureDescriptionMap.put(1, "SAL (Silver)");
-		textureDescriptionMap.put(2, "SAL (After Renovation, Late)");
-		textureDescriptionMap.put(3, "SAL (After Renovation, Silver)");
-		textureDescriptionMap.put(4, "SAL (After Renovation, Late)");
 	}
 
 	public OBBaggage_52SeatDividedCoach(World world, double posX, double posY, double posZ)
 	{
 		super(world, posX, posY, posZ);
+	}
+
+	@Override
+	public void setupTextureDescription()
+	{
+		textureDescriptionMap.put(0, "SAL");
+		textureDescriptionMap.put(1, "SAL (Silver)");
+		textureDescriptionMap.put(2, "SAL (After Renovation, Late)");
+		textureDescriptionMap.put(3, "SAL (After Renovation, Silver)");
+		textureDescriptionMap.put(4, "SAL (After Renovation, Late)");
 	}
 
 	@Override
