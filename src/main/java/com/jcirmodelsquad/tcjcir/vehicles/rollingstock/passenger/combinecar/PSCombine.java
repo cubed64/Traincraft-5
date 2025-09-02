594de677ac5c1018333d3e1bfef897b71ed057e5
TBEA <74006945+RealTBEA@users.noreply.github.com>
2025-09-02 01:09:06 -0400

    Moved PSCombine to Combine Folder

--- src/main/java/com/jcirmodelsquad/tcjcir/vehicles/rollingstock/passenger/combinecar/PSCombine.java
+++ src/main/java/com/jcirmodelsquad/tcjcir/vehicles/rollingstock/passenger/combinecar/PSCombine.java
@@ -1,4 +1,4 @@
-package com.jcirmodelsquad.tcjcir.vehicles.rollingstock.passenger;
+package com.jcirmodelsquad.tcjcir.vehicles.rollingstock.passenger.combinecar;
 
 import net.minecraft.world.World;
 import train.common.api.AbstractPassengerCar;
