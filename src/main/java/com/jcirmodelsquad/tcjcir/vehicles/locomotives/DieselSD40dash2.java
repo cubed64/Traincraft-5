cc606bcf4569d9329a25f2a8875e6925930a3135
Jared <funguseater85@gmail.com>
2025-09-07 12:25:15 -0400

    update some file
    add texture descriptions for SD40-2 and PS2 4750

--- src/main/java/com/jcirmodelsquad/tcjcir/vehicles/locomotives/DieselSD40dash2.java
+++ src/main/java/com/jcirmodelsquad/tcjcir/vehicles/locomotives/DieselSD40dash2.java
@@ -55,6 +55,7 @@ public class DieselSD40dash2 extends DieselTrain {
         textureDescriptionMap.put(33, "CSX (Yn3 Non-RCO)");
         textureDescriptionMap.put(34, "Chicago and North Western");
         textureDescriptionMap.put(35, "Seaboard System");
+        textureDescriptionMap.put(36, "Southern");
     }
     public DieselSD40dash2(World world, double d, double d1, double d2){
         this(world);
