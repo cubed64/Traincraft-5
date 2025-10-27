5998f3174c11bcd3913c330efc1d2230f64bf539
TBEA <74006945+RealTBEA@users.noreply.github.com>
2025-10-26 23:07:02 -0400

    Code Cleanup

--- src/main/java/com/jcirmodelsquad/tcjcir/vehicles/rollingstock/passenger/baggagecar/DRGWBaggage740Series.java
+++ src/main/java/com/jcirmodelsquad/tcjcir/vehicles/rollingstock/passenger/baggagecar/DRGWBaggage740Series.java
@@ -1,19 +1,8 @@
 package com.jcirmodelsquad.tcjcir.vehicles.rollingstock.passenger.baggagecar;
 
 import net.minecraft.entity.item.EntityMinecart;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.Constants;
-import train.common.Traincraft;
 import train.common.api.AbstractStandardFixedFreightCar;
-import train.common.api.Freight;
-import train.common.library.GuiIDs;
-
-import static com.sun.awt.SecurityWarning.setPosition;
 
 public class DRGWBaggage740Series extends AbstractStandardFixedFreightCar
 {
