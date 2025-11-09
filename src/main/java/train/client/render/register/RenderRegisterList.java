19f17390fde800985f4533edeb5d0ed14ba55bde
TBEA <74006945+RealTBEA@users.noreply.github.com>
2025-11-09 10:57:57 -0500

    Convert more rollingstock to TrainRecord

--- src/main/java/train/client/render/register/RenderRegisterList.java
+++ src/main/java/train/client/render/register/RenderRegisterList.java
@@ -2,6 +2,7 @@ package train.client.render.register;
 
 import train.client.render.models.*;
 import train.common.Traincraft;
+import train.common.entity.rollingStock.electric.EntityLocoElectricTramNY;
 import train.common.entity.rollingStock.passenger.*;
 import train.common.library.EnumTrainType;
 import train.common.library.Info;
@@ -45,6 +46,44 @@ public final class RenderRegisterList
                     new float[] { 0.0F, -0.44F, 0.0F },
                     new float[] { 0F, 90F, 0F },
                     null));
+
+            add(new TrainRenderRecord(modID,
+                    EntityPassenger_1class_DB.class, new ModelPassenger_1class_DB(),
+                    "passenger_1class_DB_",
+                    new float[] { 0.0F, -0.44F, 0.0F },
+                    null,
+                    null));
+            add(new TrainRenderRecord(modID,
+                    EntityPassenger_2class_DB.class, new ModelPassenger_2class_DB(),
+                    "passenger_2class_DB_",
+                    new float[] { 0.0F, -0.44F, 0.0F },
+                    null,
+                    null));
+            add(new TrainRenderRecord(modID,
+                    EntityPassengerHighSpeedCarZeroED.class, new ModelPassengerHighSpeedCarZeroED(),
+                    "passengerHighSpeedCarZeroED_",
+                    new float[] { 0.0F, -0.47F, 0.0F },
+                    null,
+                    null));
+            add(new TrainRenderRecord(modID,
+                    EntityLocoElectricTramNY.class, new ModelTramNY(),
+                    "locoTramNY_",
+                    new float[] { -1.5F, -0.44F, 0.0F },
+                    null,
+                    null));
+            add(new TrainRenderRecord(modID,
+                    EntityPassengerTramNY.class, new ModelTramNY(),
+                    "locoTramNY_",
+                    new float[] { 0F, -0.44F, 0.0F },
+                    null,
+                    null));
+            add(new TrainRenderRecord(modID,
+                    EntityPassengerAdler.class, new ModelPassengerAdler(),
+                    "passengerAdler_",
+                    new float[] { 0F, 1.04F, 0.0F },
+                    new float[] { 180F, -90F, 0F },
+                    null));
+
 
         }};
     }
--- src/main/java/train/common/library/RegisterTrains.java
+++ src/main/java/train/common/library/RegisterTrains.java
@@ -1,16 +1,23 @@
 package train.common.library;
 
 import train.common.Traincraft;
-import train.common.entity.rollingStock.passenger.EntityPassenger2;
-import train.common.entity.rollingStock.passenger.EntityPassenger5;
-import train.common.entity.rollingStock.passenger.EntityPassenger7;
-import train.common.entity.rollingStock.passenger.EntityPassengerBlue;
-import train.common.library.register.ITrainRecord;
+import train.common.entity.rollingStock.electric.*;
+import train.common.entity.rollingStock.passenger.*;
 import train.common.library.register.TrainRecord;
 
 public class RegisterTrains
 {
     public RegisterTrains()
+    {
+        RegisterRollingHeritage();
+        RegisterBAPSteam();
+        RegisterBAPDiesel();
+        RegisterBAPElectric();
+        RegisterBAPPassenger();
+        RegisterBAPFreight();
+    }
+
+    private void RegisterRollingHeritage()
     {
         Traincraft.traincraftRegistry
                 .RegisterRollingStockEntity
@@ -46,5 +53,86 @@ public class RegisterTrains
                                         18),
                                 Traincraft.instance // don't touch this line
                         );
+
+        Traincraft.traincraftRegistry
+                .RegisterRollingStockEntity
+                        (ItemIDs.minecartPassenger8_1class_DB.item,
+                                new TrainRecord("Passenger 1Class DB", EntityPassenger_1class_DB.class, ItemIDs.minecartPassenger8_1class_DB.item, EnumTrainType.Passenger, 1.5,
+                                        new String[]{"Black"},
+                                        18),
+                                Traincraft.instance // don't touch this line
+                        );
+        //passenger_1class_DB("Passenger 1Class DB", EntityPassenger_1class_DB.class, ItemIDs.minecartPassenger8_1class_DB.item, "passenger", 0, 0, 1.5, 0, 0, 0, 0, 0, 0, null, 18, 0),
+        Traincraft.traincraftRegistry
+                .RegisterRollingStockEntity
+                        (ItemIDs.minecartPassenger9_2class_DB.item,
+                                new TrainRecord("Passenger 2Class DB", EntityPassenger_2class_DB.class, ItemIDs.minecartPassenger9_2class_DB.item, EnumTrainType.Passenger, 1.5,
+                                        new String[]{"Green", "Cyan"},
+                                        18),
+                                Traincraft.instance // don't touch this line
+                        );
+
+        //passenger_2class_DB("Passenger 2Class DB", EntityPassenger_2class_DB.class, ItemIDs.minecartPassenger9_2class_DB.item, "passenger", 0, 0, 1.5, 0, 0, 0, 0, 0, 0, TraincraftUtil.getBytesFromColors(new String[]{"Green", "Cyan"}), 18, 0),
+        Traincraft.traincraftRegistry
+                .RegisterRollingStockEntity
+                        (ItemIDs.minecartPassengerHighSpeedCarZeroED.item,
+                                new TrainRecord("Passenger High Speed Zero ED", EntityPassengerHighSpeedCarZeroED.class, ItemIDs.minecartPassengerHighSpeedCarZeroED.item, EnumTrainType.Passenger, 2,
+                                        new String[]{"White", "LightGrey", "Grey"},
+                                        18),
+                                Traincraft.instance // don't touch this line
+                        );
+        //passengerHighSpeedZeroED("Passenger High Speed Zero ED", EntityPassengerHighSpeedCarZeroED.class, ItemIDs.minecartPassengerHighSpeedCarZeroED.item, "passenger", 0, 0, 2, 0, 0, 0, 0, 0, 0, TraincraftUtil.getBytesFromColors(new String[]{"White", "LightGrey", "Grey"}), 18, 0),
+
+        Traincraft.traincraftRegistry
+                .RegisterRollingStockEntity
+                        (ItemIDs.minecartNYTram.item,
+                                new TrainRecord("Loco Electric Tram NY", EntityLocoElectricTramNY.class, ItemIDs.minecartNYTram.item, EnumTrainType.Passenger, 1,
+                                        new String[] {"Grey", "White"},
+                                        18).setMHP(2428).setMaxSpeed(160).setFuelConsumption(6).setHeatingTime(170).setAccelerationRate(0.7).setBrakeRate(0.965).setBogieLocoPosition(-3),
+                                Traincraft.instance // don't touch this line
+                        );
+        Traincraft.traincraftRegistry
+                .RegisterRollingStockEntity
+                        (ItemIDs.minecartPassengerTramNY.item,
+                                new TrainRecord("Passenger Tram NY", EntityPassengerTramNY.class, ItemIDs.minecartPassengerTramNY.item, EnumTrainType.Passenger, 1,
+                                        new String[] {"Grey", "White"},
+                                        18),
+                                Traincraft.instance // don't touch this line
+                        );
+        //passengerTramNY("Passenger Tram NY", EntityPassengerTramNY.class, ItemIDs.minecartPassengerTramNY.item, "passenger", 0, 0, 1, 0, 0, 0, 0, 0, 0, TraincraftUtil.getBytesFromColors(new String[] {"Grey", "White"}), 18, 0),
+        Traincraft.traincraftRegistry
+                .RegisterRollingStockEntity
+                        (ItemIDs.minecartPassengerAdler.item,
+                                new TrainRecord("Passenger Adler", EntityPassengerAdler.class, ItemIDs.minecartPassengerAdler.item, EnumTrainType.Passenger, 0.5,
+                                        new String[] {"Black"},
+                                        18),
+                                Traincraft.instance // don't touch this line
+                        );
+        //passengerAdler("Passenger Adler", EntityPassengerAdler.class, ItemIDs.minecartPassengerAdler.item, "passenger", 0, 0, 0.5, 0, 0, 0, 0, 0, 0, null, 18, 0),
+    }
+
+    private void RegisterBAPSteam()
+    {
+        
+    }
+
+    private void RegisterBAPDiesel()
+    {
+
+    }
+
+    private void RegisterBAPElectric()
+    {
+
+    }
+
+    private void RegisterBAPPassenger()
+    {
+
+    }
+
+    private void RegisterBAPFreight()
+    {
+
     }
 }
