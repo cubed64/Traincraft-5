d33158cb2128ea5c3bc60d1649b851f8ae21851a
TBEA <74006945+RealTBEA@users.noreply.github.com>
2025-11-26 07:47:24 -0500

    Convert some more cars to use AbstractStandardFreightCar

--- src/main/java/train/common/entity/rollingStock/freight/EntityBulkheadFlatCart.java
+++ src/main/java/train/common/entity/rollingStock/freight/EntityBulkheadFlatCart.java
@@ -4,28 +4,21 @@ import com.jcirmodelsquad.tcjcir.models.loads.*;
 import com.jcirmodelsquad.tcjcir.models.trains.ModelAEM7;
 import com.jcirmodelsquad.tcjcir.models.trains.ModelOnion;
 import net.minecraft.entity.item.EntityMinecart;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.Constants;
-import train.common.Traincraft;
-import train.common.api.Freight;
+import train.common.api.AbstractStandardFreightCar;
 import train.common.entity.CargoManager;
 import train.common.entity.CargoSpecification;
-import train.common.library.GuiIDs;
 
-public class EntityBulkheadFlatCart extends Freight implements IInventory {
-
-	public int freightInventorySize;
-	public int numFreightSlots;
-	
+public class EntityBulkheadFlatCart extends AbstractStandardFreightCar
+{
 	public EntityBulkheadFlatCart(World world) {
 		super(world);
-		initBulkheadFlat();
-		setCargoManager(new CargoManager(new CargoSpecification[][] {
+	}
+
+	@Override
+	public CargoManager setupCargoManager()
+	{
+		return new CargoManager(new CargoSpecification[][] {
 				{ new CargoSpecification(ModelMixedLogs.class, "loads/MixedLogs_Oak", "Oak Logs", 0, 2.9, 0)},
 				{ new CargoSpecification(ModelMixedLogs.class, "loads/MixedLogs_Birch", "Birch Logs", 0, 2.9, 0) },
 				{ new CargoSpecification(ModelMixedLogs.class, "loads/MixedLogs_Spruce", "Spruce Logs", 0, 2.9, 0) },
@@ -68,45 +61,13 @@ public class EntityBulkheadFlatCart extends Freight implements IInventory {
 						"loads/UnwrappedWood_redwood", "Unwrapped Lumber (Redwood)", 0, 2.38, 0) },
 				{ new CargoSpecification(Modelwrappedwood_opti_brosbulk.class,//Fir
 						"loads/UnwrappedWood_fir", "Unwrapped Lumber (Fir)", 0, 2.38, 0) },
-		}));
-	}
-	
-	public void initBulkheadFlat() {
-		numFreightSlots = 9;
-		if(trainSpec!=null)freightInventorySize = trainSpec.getCargoCapacity();
-		cargoItems = new ItemStack[freightInventorySize];
-	}
-
-
-	@Override
-	protected void writeEntityToNBT(NBTTagCompound nbttagcompound) {
-		super.writeEntityToNBT(nbttagcompound);
-
-		NBTTagList nbttaglist = new NBTTagList();
-		for (int i = 0; i < cargoItems.length; i++) {
-			if (cargoItems[i] != null) {
-				NBTTagCompound nbttagcompound1 = new NBTTagCompound();
-				nbttagcompound1.setByte("Slot", (byte) i);
-				cargoItems[i].writeToNBT(nbttagcompound1);
-				nbttaglist.appendTag(nbttagcompound1);
-			}
-		}
-		nbttagcompound.setTag("Items", nbttaglist);
+		});
 	}
 
 	@Override
-	protected void readEntityFromNBT(NBTTagCompound nbttagcompound) {
-		super.readEntityFromNBT(nbttagcompound);
+	public void setupTextureDescription()
+	{
 
-		NBTTagList nbttaglist = nbttagcompound.getTagList("Items", Constants.NBT.TAG_COMPOUND);
-		cargoItems = new ItemStack[getSizeInventory()];
-		for (int i = 0; i < nbttaglist.tagCount(); i++) {
-			NBTTagCompound nbttagcompound1 = nbttaglist.getCompoundTagAt(i);
-			int j = nbttagcompound1.getByte("Slot") & 0xff;
-			if (j >= 0 && j < cargoItems.length) {
-				cargoItems[j] = ItemStack.loadItemStackFromNBT(nbttagcompound1);
-			}
-		}
 	}
 
 	@Override
@@ -115,34 +76,7 @@ public class EntityBulkheadFlatCart extends Freight implements IInventory {
 	}
 
 	@Override
-	public int getSizeInventory() {
-		return freightInventorySize;
-	}
-
-	@Override
-	public boolean interactFirst(EntityPlayer entityplayer) {
-		playerEntity = entityplayer;
-		if ((super.interactFirst(entityplayer))) {
-			return false;
-		}
-		if (!this.worldObj.isRemote) {
-			entityplayer.openGui(Traincraft.instance, GuiIDs.FREIGHT, worldObj, this.getEntityId(), -1, (int) this.posZ);
-		}
-		return true;
-	}
-
-	@Override
-	public boolean canBeRidden() {
-		return false;
-	}
-
-	@Override
 	public float getOptimalDistance(EntityMinecart cart) {
 		return 2.3F;
-	}
-	
-	@Override
-	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
-		return true;
 	}
 }
--- src/main/java/train/common/entity/rollingStock/freight/EntityFreightIceWagon.java
+++ src/main/java/train/common/entity/rollingStock/freight/EntityFreightIceWagon.java
@@ -1,17 +1,8 @@
 package train.common.entity.rollingStock.freight;
 
 import net.minecraft.entity.item.EntityMinecart;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.Constants;
-import train.common.Traincraft;
 import train.common.api.AbstractStandardFixedFreightCar;
-import train.common.api.Freight;
-import train.common.library.GuiIDs;
 
 public class EntityFreightIceWagon extends AbstractStandardFixedFreightCar
 {
@@ -29,7 +20,6 @@ public class EntityFreightIceWagon extends AbstractStandardFixedFreightCar
 	public String getInventoryName() {
 		return "Freight cart";
 	}
-
 
 	@Override
 	public float getOptimalDistance(EntityMinecart cart) {
